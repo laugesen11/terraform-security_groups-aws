1feb2958db2df69889407ee1ee8b91b0cbd78a4c
Larry <laugesen11@gmail.com>
2022-09-13 06:41:46 -0500

    Made large changes to security group setup, read variables.tf file for documentation

--- security_groups.tf
+++ security_groups.tf
@@ -34,27 +34,69 @@
 #}
 
 locals{
-  #Gets any name prefixes set in the security groups
-  security_group_name_prefixes = {
-    for item in var.security_groups: 
-      item.name => [
-        #If the string matches "name_prefix=<string>", pull the value of <string>
-        #WARNING: While this makes a list, we will only use the first value, so please do not set multiple values to prevent confusion
-        for option in item.options: 
-          chomp(trimspace(element(split("=",option),1))) if length(regexall("\\s*name_prefix\\s*=\\s*\\S",option)) > 0
-      ]
+  traffic_types = {
+    "all" = {
+      "to_port"   = 0
+      "from_port" = 0
+      "protocol"  = "all"
+    }
+
+    "http" = {
+      "to_port"   = 80
+      "from_port" = 80
+      "protocol"  = "tcp"
+    }
+
+    "https" = {
+      "to_port"   = 443
+      "from_port" = 443
+      "protocol"  = "tcp"
+    }
+
+    "ftp" = {
+      "to_port"   = 21
+      "from_port" = 21
+      "protocol"  = "tcp"
+    }
+
+    "ssh" = {
+      "to_port"   = 22
+      "from_port" = 22
+      "protocol"  = "tcp"
+    }
+
+    "telnet" = {
+      "to_port"   = 23
+      "from_port" = 23
+      "protocol"  = "tcp"
+    }
+
+    "smtp" = {
+      "to_port"   = 25
+      "from_port" = 25
+      "protocol"  = "tcp"
+    }
+
+    "web site response" = {
+      "to_port"   = 1024
+      "from_port" = 65535
+      "protocol"  = "tcp"
+    }
+
   }
- 
+
   security_group_config = {
     for item in var.security_groups: 
       item.name => {
         "name"                   = item.name
         "vpc_id"                 = lookup(var.vpcs,item.vpc,null) != null ? var.vpcs[item.vpc].vpc.id : item.vpc
-        #Check the options input to see if it contains the string "revoke_rules_on_delete". If yes, set to true. If no, set to false
-        "revoke_rules_on_delete" = item.options == null ? false : ( contains(item.options,"revoke_rules_on_delete") ? true : false )
-        #Checks if there is a name_prefix for this security group 
-        "name_prefix"            = length(local.security_group_name_prefixes[item.name]) == 0 ? null : local.security_group_name_prefixes[item.name][0]
-        "tags"                   = merge({"Name" = item.name}, item.tags)
+        "revoke_rules_on_delete" = lookup(item.options,"revoke_rules_on_delete","false") 
+        "name_prefix"            = lookup(item.options,"name_prefix",null)
+        "description"            = lookup(item.options,"description","Security group named ${item.name} for VPC ${item.vpc}")
+        "tags"                   = lookup(item.options,"tags",null) == null ? {} : {
+                                     for tag in split(",",item.options["tags"]):
+                                       element(split("=",tag),0) => element(split("=",tag),1)
+                                   }
       }
   }
 }
@@ -66,9 +108,10 @@ resource "aws_security_group" "security_groups"{
   name_prefix            = each.value.name_prefix
   vpc_id                 = each.value.vpc_id
   revoke_rules_on_delete = each.value.revoke_rules_on_delete
-  description            = "Security group ${each.value.name_prefix} for VPC ${each.value.vpc_id}"
-  tags                   = each.value.tags
+  description            = each.value.description
+  tags                   = merge({"Name" = item.name}, each.value.tags)
 }
+
 
 #Sets up the rules for each security group based on list of 'rules' objects in 'security_groups' variable
 #We create map from these values to use as inputs to the 'security_groups_rules' module
@@ -78,17 +121,33 @@ locals{
       item.name => {
         for rule in item.rules:
           "egress=${rule.is_egress} ${rule.source_type} rule from port ${rule.to_from_ports[0]} to port ${rule.to_from_ports[1]} by protocol ${rule.protocol} for security group ${item.name}" => {
-            "type"      = rule.is_egress ? "egress" : "ingress" 
-            "from_port" = rule.to_from_ports[0]
-            "to_port"   = rule.to_from_ports[1]
-            "protocol"  = rule.protocol
-            "cidr_blocks"       = lower(rule.source_type) == "ipv4" ? split(",",item.source_value) : null
-            "ipv6_cidr_blocks"  = lower(rule.source_type) == "ipv6" ? split(",",item.source_value) : null
-            "prefix_list_ids"   = lower(rule.source_type) == "prefix_list_ids" ? split(",",item.source_value) : null
-            "self"              = lower(rule.source_type) == "self" ? true : false
-            #If we specify "security_group" option, we check and see if the security group value is the name of a security groups defined here
+            "type"        = lookup(rule.rule_settings,"is_egress",null) == "true" ? "egress" : "ingress" 
+            "description" = lookup(rule.rule_settings,"description",null)
+
+            #If we set a traffic_type, we set from_port to those values. If not, we check if the protocol is icmp; if yes, we set this to the ICMP type, if no, we set it to from_port
+            "protocol"  = lookup(rule.rule_settings,"traffic_type",null) != null ? local.traffic_types[rule.rule_settings["traffic_type"]]["protocol"] : rule.rule_settings["protocol"]
+            "from_port" = lookup(rule.rule_settings,"traffic_type",null) != null ? local.traffic_types[rule.rule_settings["traffic_type"]]["from_port"] : (lower(rule.rule_settings["protocol"]) == "icmp" ? rule.rule_settings["icmp_type"] : (lookup(rule.rule_settings,"port",null) != null ? rule.rule_settings["port"] : rule.rule_settings["from_port"] ))
+
+            #If we set a traffic_type, we set to_port to those values. If not, we check if the protocol is icmp; if yes, we set this to the ICMP code, if no, we set it to "port" if defined, if not, we set to "to_port"
+            "to_port" = lookup(rule.rule_settings,"traffic_type",null) != null ? local.traffic_types[rule.rule_settings["traffic_type"]]["to_port"] : (lower(rule.rule_settings["protocol"]) == "icmp" ? rule.rule_settings["icmp_code"] : (lookup(rule.rule_settings,"port",null) != null ? rule.rule_settings["port"] : rule.rule_settings["to_port"] ))
+ 
+            #External sources settings
+            #Sets to allow access from this security group. Overrides all other settings
+            "self"              = lookup(rule.rule_settings,"self","false") 
+            #Sets the list of IPv4 addresses we allow access to. Ignored if "security_groups" or self is set
+            "cidr_blocks"       = lookup(rule.rule_settings,"self","false") == "true" || lookup(rule.rule_settings,"security_groups",null) != null ? null : ( lookup(rule.rule_settings,"cidr_blocks",null) == null ? null : tolist(split(",",rule.rule_settings["cidr_blocks"])) )
+            #Sets the list of IPv6 addresses we allow access to. Ignored if "security_groups" or self is set
+            "ipv6_cidr_blocks"  = lookup(rule.rule_settings,"self","false") == "true" || lookup(rule.rule_settings,"security_groups",null) != null ? null : ( lookup(rule.rule_settings,"ipv6_cidr_blocks",null) == null ? null : tolist(split(",",rule.rule_settings["ipv6_cidr_blocks"])) )
+            #Sets the list of prefix list ids we allow access to. Ignored if "security_groups" or self is set
+            "prefix_list_ids"   = lookup(rule.rule_settings,"self","false") == "true" || lookup(rule.rule_settings,"security_groups",null) != null ? null : ( lookup(rule.rule_settings,"prefix_list_ids",null) == null ? null : tolist(split(",",rule.rule_settings["prefix_list_ids"])) )
+
+            #If we specify "security_groups" option, we check and see if the security group value is the name of a security groups defined here
             #If not, we assume this is the external ID of a security group
             "source_security_group_id" = lower(rule.source_type) != "security_group" ? null : (lookup(aws_security_group.security_groups,item.source_value,null) != null ? aws_security_group.security_groups[item.source_value].id : item.source_value) 
+            "source_security_group_id" = lookup(rule.rule_settings,"self","false") == "true" ? null : ( lookup(rule.rule_settings,"security_groups",null) == null ? null : [
+              for security_group in split(rule.rule_settings["security_groups"]):
+                lookup(aws_security_group.security_groups,security_group,null) != null ? aws_security_group.security_groups[security_group].id : security_group
+            ])
           }
       }
   }
--- variables.tf
+++ variables.tf
@@ -10,31 +10,41 @@ variable "security_groups" {
       #This value will NOT be used to name security group if you set value for 'name_prefix'
       name                   = string
       vpc                    = string
+
       #Sets optional values. Expected values:
-      #  #Sets the name prefix. Otherwise, security group name is set using 'name' value
-      #  "name_prefix=<string>"
-      #  #Makes it so rules are revoked on deletion
-      #  "revoke_rules_on_delete"
-      options                = list(string)
-      tags                   = map(string)
+      #  - "name_prefix"="<string>" - Sets the name prefix. Otherwise, security group name is set using 'name' value
+      #  - "description"="<string>" - optional description
+      #  - "revoke_rules_on_delete"=<true|false> - Makes it so rules are revoked on deletion
+      #  - tags="<tag_name1>=<tag_value1>,<tag_name2>=<tag_value2>,..."
+      options                = map(string)
       rules                  = list(
         object({ 
-          is_egress                   = bool
-          #Make a list with the to_port and from_port values
-          #expected value: list(<from_port>,<to_port>)
-          to_from_ports               = list(number)
-          protocol                    = string
-
-          #variable 'source_type' can be set to the below values:
-          #  - ipv4            - specify a IPv4 CIDR Block
-          #  - ipv6            - specify a IPv6 CIDR Block
-          #  - prefix_list_ids - specify a comma separated list of prefix list IDs
-          #  - self            - use security group ID this rule is assigned to as a source for the ingress rule
-          #  - security_group  - use the name of a security group created in this module or and external security group ID as the source
-          source_type = string
-
-          #Source value can only be set to null if 'source_type' is set to 'self'
-          source_value = string
+       
+          #Sets the options for rules
+          #  - "is_egress"=<true|false> - sets the rule to be egress. Is ingress by default
+          #  - "description"="<string>" - optional description
+          # The below settings need to be put in place to ensure the correct traffic type is specified
+          # Specifying a "traffic_type" will resolve the protocol and port range (from_port,to_port)
+          # Leave this undefined and specify the protocol and ports yourself if you have a custom setup in mind
+          #  - "traffic_type"=<string> - sets the traffic to a set of predefined values.
+          #  - "protocol"=<protocol name or number> - set to icmp, icmpv6, tcp, udp, all, or the protocol number
+          #  - "port"=<port number> - sets the port number if we only want to use one. Will set from_port and to_port to be equal
+          #  - "from_port"=<beginning of port range> - lower bound of port range. Will set to same as "to_port" if not set
+          #  - "to_port"=<end of port range> - upper bound of port range. Will set to same as "from_port" if not set
+          # Only use these values if you set the protocol to icmp or icmpv6. Otherwise these are ignore
+          #  - "icmp_type"=<ICMP type number> - sets the icmp type if the protcol is set to icmp
+          #  - "icmp_code"=<ICMP code> - sets the icmp code if the protcol is set to icmp
+          # Settings for the external traffic this rule is allowing
+          # NOTE: remember, security groups can only ALLOW traffic, they cannot deny it
+          # "cidr_blocks","ipv6_cidr_blocks", and "prefix_list_ids" can all be used together
+          #  - "cidr_blocks"=<comma separated list of IPv4 CIDR blocks> 
+          #  - "ipv6_cidr_blocks"=<comma separated list of IPv4 CIDR blocks> 
+          #  - "prefix_list_ids"=<comma separated list of prefix list ids> 
+          # WARNING: "security_groups" will cause "cidr_blocks","ipv6_cidr_blocks", and "prefix_list_ids" to be ignore. 
+          #  - "security_groups"=<comma separated values of security group names or IDs> - can use security group names from this module
+          # WARNING:"self" can only be used alone. Setting this will ignore "security_groups", "cidr_blocks","ipv6_cidr_blocks", and "prefix_list_ids"
+          #  - "self"=<true|false> - uses this security group as the entity allowed access
+          rule_settings = map(string)
         })
       )
     })
