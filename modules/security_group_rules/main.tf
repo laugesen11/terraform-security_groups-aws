a1443319e28b698e9d6ff41d53092b583e794588
Larry <laugesen11@gmail.com>
2022-09-27 14:57:31 -0500

    Trying dynamic blocks

--- modules/security_group_rules/main.tf
+++ modules/security_group_rules/main.tf
@@ -10,9 +10,6 @@ resource "aws_security_group_rule" "security_group_rules" {
   cidr_blocks              = each.value.cidr_blocks == null ? null : split(",",each.value.cidr_blocks)
   ipv6_cidr_blocks         = each.value.ipv6_cidr_blocks == null ? null : split(",",each.value.ipv6_cidr_blocks) 
   prefix_list_ids          = each.value.prefix_list_ids == null ? null : split(",",each.value.prefix_list_ids)
-#  cidr_blocks              = each.value.cidr_blocks 
-#  ipv6_cidr_blocks         = each.value.ipv6_cidr_blocks 
-#  prefix_list_ids          = each.value.prefix_list_ids 
   self                     = each.value.self
   source_security_group_id = each.value.source_security_group_id 
 }
